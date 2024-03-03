702add98d5aa447c1322f6596e78cfd3eaf5c3f4
pawelZablotny <161207593+pawelZablotny@users.noreply.github.com>
2024-03-03 11:11:01 +0100

    zmiany

--- cw2.py
+++ cw2.py
@@ -37,6 +37,55 @@ import sys
 
 # print('liczba a jest rowna {:.2f}, liczba b jest rowna {:d}'.format(a, b))
 
-a = input('Wprowadz liczbe: ')
-print(a)
-print(type(a))
+# a = input('Wprowadz liczbe: ')
+# print(a)
+# print(type(a))
+# a = int(a)
+# print(a*5)
+# print(type(a))
+
+# sys.stdout.write('Wprowadz liczbe: ')
+# b = sys.stdin.readline()
+# print(b)
+# print(type(b))
+
+# lista = [5, 6, 7, 8.8, 42, 'a', 'b', [2, 3, 4], 'ab']
+# print(lista)
+# lista.append(67)
+# print(lista)
+# lista.insert(2, 'c')
+# print(lista)
+# lista.extend([20, 21, 22])
+# print(lista)
+# lista.pop()
+# print(lista)
+# lista.pop(2)
+# print(lista)
+# lista.remove([2, 3, 4])
+# print(lista)
+# del lista[1]
+# print(lista)
+# lista.reverse()
+# print(lista)
+# lista.sort()
+# print(lista)
+
+# slownik = {'klucz': 'wartosc', 1: 2, 'a': 5, 4: 'b', 1: 10}
+# print(slownik)
+# print(slownik[4])
+# slownik[6] = 45
+# print(slownik)
+# slownik.pop(1)
+# print(slownik)
+# print(slownik.keys())
+# print(slownik.values())
+# del slownik[6]
+
+a = 7
+b = 7
+if a > b:
+    print("a is greater than b")
+elif a < b:
+    print("b is less than b")
+else:
+    print("b is greater than a")
