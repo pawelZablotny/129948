cb643bede6018e859a1c0d871f586acc869a68f5
pawelZablotny <161207593+pawelZablotny@users.noreply.github.com>
2024-03-03 10:40:15 +0100

    zmiany

--- cw2.py
+++ cw2.py
@@ -1,3 +1,42 @@
 import sys
+# a = 56
+# print(a)
+# print(type(a))
+# b = 5.5
+# print(b)
+# print(type(b))
+# zmienna_tekstowa = 'wizualizacja danych'
+# print(zmienna_tekstowa)
+# print(type(zmienna_tekstowa))
 
-print(sys.version)
+# a = 6
+# b = 3
+# c = a + b
+# print(c)
+# d = a - b
+# print(d)
+# e = 4
+# f = b // a
+# print(f)
+# f =  a // b
+# print(f)
+# g = a ** 2
+# print(g)
+# h = pow(a, 2)
+# print(h)
+#
+# i = 6 ** (1/2)
+# j = pow(6, 1/2)
+# print(i)
+# print(j)
+#
+# k = 'wizualizacje danych'
+# l = ' grupa '
+# m = 2
+# print(k + l + str(m))
+
+# print('liczba a jest rowna {:.2f}, liczba b jest rowna {:d}'.format(a, b))
+
+a = input('Wprowadz liczbe: ')
+print(a)
+print(type(a))
