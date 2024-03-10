19a7e185657b6ea9a9e427d13dd6da985e353727
pawelZablotny <161207593+pawelZablotny@users.noreply.github.com>
2024-03-10 11:16:31 +0100

    zmiany

---
+++ cw3.py
@@ -0,0 +1,47 @@
+# try:
+#
+#     a = int(input())
+#     b = int(input())
+#     result = a/b
+#    print(result)
+#
+# except ZeroDivisionError:
+#     print("Error")
+#
+# except ValueError:
+#     print("wrong value")
+# else:
+#     print(result)
+
+# except Exception:
+#     print("Error")
+# else:
+#     print(result)
+
+# list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# l2 = []
+# for i in list1:
+#     l2.append(pow(i, 2))
+# print(l2)
+# l2 = [x**2 for x in list1]
+# print(l2)
+# l3 = [3**y for y in range(0, 6)]
+# print(l3)
+# l4 = [x for x in l2 if x%2 == 1]
+# print(l4)
+# l5 = [(x, y) for x  in l3 for y in l4]
+# print(l5)
+# l6 = []
+# for x in l3:
+#     for y in l4:
+#         l6.append((x, y))
+# print(l6)
+
+s1 = {1: 2, 2: 3, 3: 4, 4: 5}
+s2 = {}
+for key, value in s1.items():
+    s2[value] = key
+print(s1)
+print(s2)
+s3 = {v: k for k, v in s1.items()}
+print(s3)
