1a607ad4be081128fe3f3b874505f0b9f8297cc1
pawelZablotny <161207593+pawelZablotny@users.noreply.github.com>
2024-03-10 12:12:40 +0100

    zmiany

--- cw2plik.py
+++ cw2plik.py
@@ -1,10 +1,27 @@
-plik = open('tekst.txt', 'r', encoding='utf-8')
-znaki = plik.read(10)
+# plik = open('tekst.txt', 'r', encoding='utf-8')
+# znaki = plik.read(10)
+#
+# linia = plik.readline()
+# linie = plik.readlines()
+# plik.close()
+# print(znaki)
+# print(linia)
+# print(linie)
+#
+# for l in linie:
+#     print(l)
 
-linia = plik.readline()
-linie = plik.readlines()
-plik.close()
-print(znaki)
-print(linia)
-print(linie)
+# plik = open('tekst1.txt', 'w')
+# plik.write('ITS OVER 9000')
+# plik.close()
 
+# plik = open('tekst.txt', 'a+')
+# plik.write('ITS OVER 9000')
+# plik.seek(105)
+# znaki = plik.read(10)
+# plik.close()
+# print(znaki)
+
+with open('tekst.txt', 'r') as f:
+    lines = f.readlines()
+print(lines)
