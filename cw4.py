fc4119cdb77d371761dde9a5c58ece9e5c65bd9f
pawelZablotny <161207593+pawelZablotny@users.noreply.github.com>
2024-03-24 12:32:59 +0100

    zmiany

--- cw4.py
+++ cw4.py
@@ -52,16 +52,76 @@ import numpy as np
 # liczby_lin = np.linspace(1, 2, 5, endpoint=False)
 # print(liczby_lin)
 
-z = np.indices((5, 3))
-print(z)
-print(z[0, 3, 1])
-x, y = np.mgrid[0:5, 0:5]
+# z = np.indices((5, 3))
+# print(z)
+# print(z[0, 3, 1])
+# x, y = np.mgrid[0:5, 0:5]
+# print(x)
+# print(y)
+#
+# mat_diag_k = np.diag([a for a in range(5)], k=-1)
+# print(mat_diag_k)
+#
+# z = np.fromiter(range(5), dtype='int32')
+# print(z)
+
+# znaki = b'adcbdef'
+# zn1 = np.frombuffer(znaki, dtype='S1')
+# print(zn1)
+# zn2 = np.frombuffer(znaki, dtype='S4')
+# print(zn2)
+
+# znaki = 'abcdef'
+# zn3 = np.array(list(znaki))
+# zn4 = np.array(list(znaki), dtype='S1')
+# zn5 = np.array(list(b'abcdef'))
+# zn6 = np.fromiter(znaki, dtype='S1')
+# zn7 = np.fromiter(znaki, dtype='U1')
+#
+# print(zn3)
+# print(zn4)
+# print(zn5)
+# print(zn6)
+# print(zn7)
+
+# mat = np.ones((2, 2))
+# mat_1 = np.ones((2, 2))
+# mat = mat + mat_1
+# print(mat)
+# print(mat - mat_1)
+# print(mat * mat_1)
+# print(mat/mat_1)
+# mat_1 = np.array([[3, 5], [5, 4]])
+# print(mat_1)
+# print(mat*mat_1)
+
+# a = np.arange(10)
+# print(a)
+# s = slice(2, 7, 2)
+# print(a[s])
+# s = range(2, 7, 2)
+# print(a[s])
+# print(a[2:7:2])
+# print(a[1:])
+# print(a[2:5])
+
+# mat = np.arange(25)
+#
+# mat = mat.reshape((5, 5))
+# print(mat)
+# print(mat[1:])
+# print(mat[:, 1])
+# print(mat[:, -1:])
+# print(mat[2:5, 1:3])
+# print(mat[:, range(2,6,2)])
+# print('')
+
+x = np.array([[0, 1, 2],
+              [3, 4, 5],
+              [6, 7, 8],
+              [9, 10, 11]])
 print(x)
+rows = np.array([[0, 0], [3, 3]])
+cols = np.array([[0, 2], [0, 2]])
+y = x[rows, cols]
 print(y)
-
-mat_diag_k = np.diag([a for a in range(5)], k=-1)
-print(mat_diag_k)
-
-z = np.fromiter(range(5), dtype='int32')
-print(z)
-
