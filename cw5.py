2584766511305530f13992623e527f127861995a
pawelZablotny <161207593+pawelZablotny@users.noreply.github.com>
2024-05-26 15:26:42 +0200

    zmiany

--- cw5.py
+++ cw5.py
@@ -2,26 +2,88 @@ import numpy as np
 
 # a = np.array([20, 30, 40, 50])
 # b = np.arange(4)
-# print(a)
-# print(b)
+# # print(a)
+# # print(b)
 #
 # c = a - b
-# print(c)
+# # print(c)
 #
-# print(b**2)
+# # print(b**2)
 #
-# print(a)
+# # print(a)
 #
 # a += b
-# print(a)
+# # print(a)
 #
 # a -= b
 #
-# print(a)
+# # print(a)
+#
+# d = a*b
+# print(d)
+#
+# b = np.arange(3)
+# print(b)
+# print(np.exp(b))
+# print(np.sqrt(b))
+# c = np.array([2., -1., 4.])
+# print(np.add(b, c))
 
-b = np.arange(3)
-print(b)
-print(np.exp(b))
-print(np.sqrt(b))
-c = np.array([2., -1., 4.])
-print(np.add(b, c))
+# a = np.arange(3)
+# b = np.arange(3)
+# print(a)
+# print(b)
+# print(a.dot(b))
+# print(np.dot(a,b))
+# c = np.array([[1, 5], [2, 6], [7, 4], [5, 2]])
+# d = np.array([[2, 5, 4], [4, 3, 1]])
+# print(c)
+# print(d)
+# print(np.dot(c,d))
+# print(np.dot(d,c))
+
+# a = np.arange(12).reshape((3,4))
+# print(a)
+# print(a.sum())
+# print(a.sum(axis=0))
+# print(a.min(axis=1))
+# print(a.cumsum(axis=1))
+#
+# a = np.arange(6).reshape((3,2))
+# print(a)
+# for b in a:
+#     print(b)
+#
+# a = np.arange(6).reshape((3,2))
+# print(a)
+# for b in a.flat:
+#     print(b)
+
+# a = np.arange(6).reshape((3,2))
+# print(a)
+# print(a.shape)
+# print(type(a.shape))
+# for b in range(0, a.shape[0]):
+#     for j in range(0, a.shape[1]):
+#         print(a[i][j], end='')
+#     print(" ")
+
+# a = np.arange(6)
+# print(a)
+# print(a.shape)
+# print("")
+# b = a.reshape((2,3))
+# print(b)
+# print(b.shape)
+# print("")
+# c = b.reshape((3,2))
+# print(c)
+# print(c.shape)
+# print("")
+# d = c.ravel()
+# print(d)
+# print(d.shape)
+# print("")
+# e = b.T
+# print(e)
+# print(e.shape)
