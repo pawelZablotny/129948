94287540ad3fbfbd866e6e72531e826799ab4037
pawelZablotny <161207593+pawelZablotny@users.noreply.github.com>
2024-05-26 14:37:52 +0200

    zmiany

--- cw5.py
+++ cw5.py
@@ -1,16 +1,27 @@
 import numpy as np
 
-a = np.array([20, 30, 40, 50])
-b = np.array(4)
-print(a)
+# a = np.array([20, 30, 40, 50])
+# b = np.arange(4)
+# print(a)
+# print(b)
+#
+# c = a - b
+# print(c)
+#
+# print(b**2)
+#
+# print(a)
+#
+# a += b
+# print(a)
+#
+# a -= b
+#
+# print(a)
+
+b = np.arange(3)
 print(b)
-
-c = a - b
-print(c)
-
-print(b**2)
-
-print(a)
-
-a += b
-print(a)
+print(np.exp(b))
+print(np.sqrt(b))
+c = np.array([2., -1., 4.])
+print(np.add(b, c))
