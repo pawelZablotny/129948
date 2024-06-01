64c1a56161f0bfc0783ca873e30f2be621cb34b6
pawelZablotny <161207593+pawelZablotny@users.noreply.github.com>
2024-06-01 11:46:58 +0200

    zmiany

--- cw5pandas.py
+++ cw5pandas.py
@@ -8,31 +8,75 @@ data = {'Kraj': ['Belgia','Indie','Brazylia'],
         'Stolica':['Bruksela','New Delhi','Brasilia'],
         'Populacja': [11190846,1303171035,207847528]}
 df = pd.DataFrame(data)
+# print(df)
+# print(df.dtypes)
+# # df = pd.read_csv('dane.csv', header=0, sep=";", decimal='.')
+# print(df)
+# df.to_csv('plik.csv', index=False)
+#
+# print(s['c'])
+# print(s.c)
+# print(df[0:1])
+# print("")
+# print(df['Populacja'])
+# print(df.iloc[0, 0])
+# print(df.loc[0, "Kraj"])
+# print(df.at[0, "Kraj"])
+#
+# ##Ćw6
+#
+# print('kraj:' + df.Kraj)
+# print(df.sample())
+#
+# print(df.sample(2))
+# print(df.sample(frac=0.5))
+# print(df.sample(n=10, replace=True))
+# print(df.head())
+# print(df.head(2))
+# print(df.tail(1))
+# print(df.describe())
+# print(df.T)
+
+# print(s[(s>9)])
+# print(s.where(s > 10))
+# print(s.where(s>10, 'za duze'))
+# seria = s.copy()
+# seria.where(seria > 10, 'za duze', inplace=True)
+# print('###########')
+# print(seria)
+#
+# print(s[~(s > 10)])
+# print(s[(s < 13) & (s > 8)])
+
+# print(df[(df.Populacja > 1000000) & (df.index.isin([0,2]))])
+#
+# print('########')
+# szukaj=['Belgia', 'Brasilia']
+# print(df.isin(szukaj))
+
+
+s['e'] = 15
+print(s.e)
+s['f'] = 16
+print(s)
+
+df.loc[3] = 'dodane'
 print(df)
-print(df.dtypes)
-# df = pd.read_csv('dane.csv', header=0, sep=";", decimal='.')
+df.loc[4] = ['Polska', 'Warszawa', 38675367]
 print(df)
-df.to_csv('plik.csv', index=False)
 
-print(s['c'])
-print(s.c)
-print(df[0:1])
-print("")
-print(df['Populacja'])
-print(df.iloc[0, 0])
-print(df.loc[0, "Kraj"])
-print(df.at[0, "Kraj"])
+new_df = df.drop([3])
+print(new_df)
 
-##Ćw6
+df.drop([3], inplace=True)
+print(df)
 
-print('kraj:' + df.Kraj)
-print(df.sample())
+# df.drop('Kraj, axis=1, inplace=True)
+df['Kontynent'] = ['Europa', 'Azja', 'Ameryka Poludniowa', 'Europa']
 
-print(df.sample(2))
-print(df.sample(frac=0.5))
-print(df.sample(n=10, replace=True))
-print(df.head())
-print(df.head(2))
-print(df.tail(1))
-print(df.describe())
-print(df.T)
+print(df)
+
+print(df.sort_values(by='Kraj'))
+grouped = df.groupby('Kontynent')
+print(grouped.get_group('Europa'))
+print(df.groupby(['Kontynent']).agg({'Populacja':['sum']}))
