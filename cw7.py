b13f0e108bb4b70fbd5624629b2fe7cc36bbd7a9
pawelZablotny <161207593+pawelZablotny@users.noreply.github.com>
2024-06-08 14:50:15 +0200

    zmiany

--- cw7.py
+++ cw7.py
@@ -151,3 +151,101 @@ import matplotlib.pyplot as plt
 # plt.legend(fuel_percentages.index, title="Rodzaj paliwa", fontsize=14)
 #
 # plt.show()
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Zad1
+
+#Na wykresie wyświetl wykres liniowy funkcji f(x) = 1/x dla x ϵ [1, 20]. Dodaj etykietę do linii wykresu i wyświetl legendę.
+#Dodaj odpowiednie etykiety do osi wykresu (‘x’, ’f(x)’) oraz ustaw zakres osi na (0, 1) oraz (1, długość wektora x).
+
+# x = np.arange(1, 21, 1)
+# plt.plot(x, 1/x, label='f(x) = 1/x')
+# plt.xlabel('x')
+# plt.ylabel('f(x)')
+# plt.axis((0, 20, 0, 1))
+# plt.legend()
+# plt.title('Wykres funkcji f(x) = 1/x dla x[1,20]')
+# plt.show()
+
+#Zad2
+
+#Zmodyfikuj wykres z zadania 1 tak, żeby styl wykresu wyglądał tak jak na poniższym zrzucie ekranu.
+
+# x = np.arange(1, 21, 1)
+# plt.plot(x, 1/x, 'g>:', label='f(x) = 1/x' )
+# plt.xlabel('x')
+# plt.ylabel('f(x)')
+# plt.axis((0, 20, 0, 1))
+# plt.legend()
+# plt.title('Wykres funkcji f(x) = 1/x dla x[1,20]')
+# plt.show()
+
+#Zad3
+
+#Na jednym wykresie wygeneruj wykresy funkcji sin(x) oraz cos(x) dla x ϵ [0, 30] z krokiem 0.1.
+#Dodaj etykiety i legendę do wykresu.
+
+# x = np.arange(0, 30.1, .1)
+# plt.plot(x, np.sin(x), 'b', label='sin(x)')
+# plt.plot(x, np.cos(x), 'r', label='cos(x)')
+# plt.xlabel('x')
+# plt.ylabel('sin(x) cos(x)')
+# plt.legend(loc='upper right')
+# plt.title('Wykres sin(x), cos(x)')
+# plt.show()
+
+#Zad4
+
+#Korzystając ze zbioru danych Iris (https://archive.ics.uci.edu/ml/datasets/iris) wygeneruj wykres punktowy,
+#gdzie wektor x to wartość ‘sepal length’ a y to ‘sepal width’, dodaj paletę kolorów c na przykładzie listingu 6
+#a parametr s niech będzie wartością absolutną z różnicy wartości poszczególnych elementów wektorów x oraz y.
+
+# df = pd.read_csv('iris.data', header=0, sep=',', decimal='.')
+# print(df)
+# # przygotowanie wektora kolorów
+# colors = np.random.randint(0, 50, len(df.index))
+# # przygotowanie wektora z rozmiarami 'kropek'
+# scale = np.abs(df['sepal length'] - df['sepal width'])
+
+#Zad5
+
+#Korzystając z biblioteki pandas wczytaj zbiór danych z narodzinami dzieci przedstawiony w lekcji 8.
+#Następnie na jednym płótnie wyświetl 3 wykresy (jeden wiersz i 3 kolumny). Dodaj do wykresów stosowne etykiety.
+#Poustawiaj różne kolory dla wykresów.
+#1 wykres – wykres słupkowy przedstawiający ilość narodzonych dziewczynek i chłopców w całym okresie.
+#2 wykres – wykres liniowy, gdzie będą dwie linie, jedna dla ilości urodzonych kobiet,
+#druga dla mężczyzn dla każdego roku z osobna. Czyli y to ilość narodzonych kobiet lub mężczyzn (dwie linie), x to rok.
+#3 wykres – wykres słupkowy przedstawiający sumę urodzonych dzieci w każdym roku.
+
+
+# xlsx = pd.ExcelFile('imiona.xlsx')
+# df = pd.read_excel(xlsx, header=0)
+# print(df)
+# plt.subplot(1, 3, 1)
+# grouped = df.groupby('Plec')
+# etykiety = np.array(list(grouped.groups.keys()))
+# wartosci = list(grouped.agg('Liczba').sum())
+# plt.bar(x=etykiety, height=wartosci, color=['green', 'red'])
+# plt.xlabel('Płeć')
+# plt.ylabel('Liczba narodzonych dzieci')
+# # wykres 2
+# plt.subplot(1, 3, 2)
+# x = df['Rok'].unique()
+# kobiety = df[(df.Plec == 'K')].groupby('Rok').agg({'Liczba':['sum']}).values
+# mezczyzni = df[(df.Plec == 'M')].groupby('Rok').agg({'Liczba':['sum']}).values
+# plt.plot(x, kobiety, label="Kobiety")
+# plt.plot(x, mezczyzni, label="Mężczyźni")
+# plt.xlabel('Rok')
+# #
+# # # wykres 3
+# plt.subplot(1, 3, 3)
+# x = df['Rok'].unique()
+# y = df.groupby('Rok').agg('Liczba').sum()
+# plt.bar(x, y)
+# plt.xlabel('Rok')
+# # wyświetlamy cały wykres
+# plt.subplots_adjust(wspace=0.85)
+# plt.show()
